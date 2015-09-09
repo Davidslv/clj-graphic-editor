dc98d3b4d6b1c664f53f2d92a4f7b4de91e63d3d
David Silva <davidslv@gmail.com>
2015-09-09 17:35:00 +0100

    Adds test cases for colour-pixel function

--- test/graphic_editor/core_test.clj
+++ test/graphic_editor/core_test.clj
@@ -6,5 +6,12 @@
   (testing "it should return a new image"
     (is (= (new-image 2 2) [[\O \O] [\O \O]])))
 
-    (testing "it should return a 5x5 new image"
+  (testing "it should return a 5x5 new image"
     (is (= (new-image 5 5) [[\O \O \O \O \O] [\O \O \O \O \O] [\O \O \O \O \O] [\O \O \O \O \O] [\O \O \O \O \O]]))))
+
+(deftest colour-pixel-test
+  (testing "it starts with zero index"
+    (is (= (colour-pixel (new-image 4 5) 0 0 \A) [[\A \O \O \O \O] [\O \O \O \O \O] [\O \O \O \O \O] [\O \O \O \O \O]])))
+
+  (testing "it should colour the given pixel"
+    (is (= (colour-pixel (new-image 4 5) 1 2 \A) [[\O \O \O \O \O] [\O \O \A \O \O] [\O \O \O \O \O] [\O \O \O \O \O]]))))
