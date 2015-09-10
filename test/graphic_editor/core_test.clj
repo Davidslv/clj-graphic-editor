852ebc44c5414f3d0c4123a0d4a3f6b696dfd58b
David Silva <davidslv@gmail.com>
2015-09-10 19:06:05 +0100

    test case for 2nd column
    - There's an edge case that needs to be looked at:
      When the row coordinate is for example the second, it will only
      substitute from the second row to the bottom, leaving the first
      row unchanged.

--- test/graphic_editor/core_test.clj
+++ test/graphic_editor/core_test.clj
@@ -18,4 +18,7 @@
 
 (deftest colour-column-test
   (testing "it should colour the first column"
-    (is (= (colour-column (new-image 3 3) 0 0 \A) [[\A \O \O] [\A \O \O] [\A \O \O]]))))
+    (is (= (colour-column (new-image 3 3) 0 0 \A) [[\A \O \O] [\A \O \O] [\A \O \O]])))
+
+  (testing "it should colour the second column"
+    (is (= (colour-column (new-image 3 3) 0 1 \A) [[\O \A \O] [\O \A \O] [\O \A \O]]))))
